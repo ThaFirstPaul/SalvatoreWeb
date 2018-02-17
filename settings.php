652671a4e3e8ed0643385eb4b972540fa6200775
BuildTools <unconfigured@null.spigotmc.org>
2018-02-17 13:40:01 +0300

    Added more checks to settings.php

--- settings.php
+++ settings.php
@@ -1,7 +1,6 @@
-<?php session_start(); ?>
+<?php session_start(); if (!isset($_SESSION["username"])) { header("Location: index.php"); } ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html><head>
-
 
   
   <link rel="stylesheet" href="css/bootstrap.min.css"><title>Salvatore Guild - Personal Settings</title></head><body>
