eb14ae1b1fefedfcdabfa968b417ec0a1c04b7b1
ThaFirstPaul <34832378+ThaFirstPaul@users.noreply.github.com>
2018-02-19 00:35:44 +0300

    Update sendmessage.php

--- sendmessage.php
+++ sendmessage.php
@@ -1,4 +1,8 @@
-<?php session_start(); if (!isset($_SESSION["username"])) { header("Location: index.php")} require("api/api.php");
+<?php session_start();
+if (!isset($_SESSION["username"])) {
+    header("Location: index.php")
+}
+require("api/api.php");
 
 if ($_POST["message"] !== "") {
     if (!send_message($_SESSION["username"], $_POST["message"])){
