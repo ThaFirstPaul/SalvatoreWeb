2ebdf95f7175d8e4850ccfa02952e1fb789d43ba
ThaFirstPaul <34832378+ThaFirstPaul@users.noreply.github.com>
2018-02-19 07:34:06 +0300

    Update success.php

--- success.php
+++ success.php
@@ -7,7 +7,11 @@
 <div class="container">
 <h1 id="title">Success!<br>
 </h1>
-<p><?php if(isset($_SESSION['change'])){echo $_SESSION['change']; session_destroy();} else { echo "Your changes have been successfully saved!";} ?>
+<p><?php if(isset($_SESSION['change'])){
+  echo $_SESSION['change'];
+} else { 
+  echo "Your changes have been successfully saved!";
+} ?>
 <br>
 </p>
 </div>
@@ -18,7 +22,10 @@
 <?php $a = "window.location.href='/{$_SESSION['goto']}.php'"; ?>
 <button onclick=<?php echo $a; ?> type="submit" class="btn btn-primary">
 <?php if($_SESSION['goto'] === "index"){
-    echo "To Homepage";
+    echo "Back to Homepage";
+} elseif ($_SESSION['goto'] === "logout"){
+  session_destroy();
+  echo "Back to Homepage";
 } else {
     echo "Back to "; echo $_SESSION['goto']; }?>
 </button>
