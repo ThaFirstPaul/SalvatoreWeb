c5d8be15d7c8c5a45fa9b22254e30933cfb80244
ThaFirstPaul <34832378+ThaFirstPaul@users.noreply.github.com>
2018-02-20 12:43:44 +0300

    Update messageboard.php

--- messageboard.php
+++ messageboard.php
@@ -1,31 +1,31 @@
 <?php if (isset($_SESSION["username"])) { ?>
 
 <br><hr><br>
-    <div class="container">
-<h2>Messages:</h2>
-<form id="form" method="post" action="message.php">
-<input id="message" name="message" placeholder="Message" class="form-control">
-<button id= "sendmessage" name="deletesure" type="submit" class="btn btn-primary">Send</button>
-</form>
+<div class="container">
+    <h2>Messages:</h2>
+    <form id="form" method="post" action="message.php">
+        <input id="message" name="message" placeholder="Message" class="form-control">
+        <button id= "sendmessage" name="deletesure" type="submit" class="btn btn-primary">Send</button>
+    </form>
 </div>
+
 <?php } else {
     echo "<h4>Sign in to send messages!</h4>";
-}?>
+} ?>
 
 <br>
 
 <div class="container">
 <table class="table table-bordered">
-<tr>
-<th>Username</th>
-<th>Message</th>
-<?php if (getrank($_SESSION["username"]) === "administrator") { echo "<th>Delete</th>";} ?>
-</tr>
+    <tr>
+        <th>Username</th>
+        <th>Message</th>
+        <?php if (getrank($_SESSION["username"]) === "administrator") { echo "<th>Delete</th>";} ?>
+    </tr>
 
 <?php
     $messages = get_messages();
-    while ($message = $messages->fetch_assoc()) {
-        ?>
+    while ($message = $messages->fetch_assoc()) { ?>
 <tr>
 <td><?php echo $message["username"] ?></td>
 <td><?php echo $message["message"] ?></td>
@@ -39,5 +39,4 @@
 
 </tr>
 </table>
-</div>
 </div>
