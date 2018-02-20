7bf63f72f6e2f0725df09452f8333b71c26d443a
ThaFirstPaul <paul.von.lutzow@gmail.com>
2018-02-20 15:59:59 +0300

    Small fixes

--- messageboard.php
+++ messageboard.php
@@ -2,11 +2,11 @@
 
 <br><hr><br>
 <div class="container">
-    <h2>Messages:</h2>
-    <form id="form" method="post" action="message.php">
-        <input id="message" name="message" placeholder="Message" class="form-control">
-        <button id= "sendmessage" name="deletesure" type="submit" class="btn btn-primary">Send</button>
-    </form>
+<h2>Messages:</h2>
+<form id="form" method="post" action="message.php">
+<input id="message" name="message" placeholder="Message" class="form-control">
+<button id= "sendmessage" name="deletesure" type="submit" class="btn btn-primary">Send</button>
+</form>
 </div>
 
 <?php } else {
@@ -17,11 +17,11 @@
 
 <div class="container">
 <table class="table table-bordered">
-    <tr>
-        <th>Username</th>
-        <th>Message</th>
-        <?php if (getrank($_SESSION["username"]) === "administrator") { echo "<th>Delete</th>";} ?>
-    </tr>
+<tr>
+<th>Username</th>
+<th>Message</th>
+<?php if (getrank($_SESSION["username"]) === "administrator") { echo "<th>Delete</th>";} ?>
+</tr>
 
 <?php
     $messages = get_messages();
@@ -30,13 +30,16 @@
 <td><?php echo $message["username"] ?></td>
 <td><?php echo $message["message"] ?></td>
 <?php if (getrank($_SESSION["username"]) === "administrator") { ?>
-    <td>
-        <form id="form" method="post" action="message.php">
-        <button type="submit" name="deletemessage" class="btn btn-danger">Delete</button>
-        </form>
-    </td>
+<td>
+<form id="form" method="post" action="message.php">
+<button type="submit" name="deletemessage" class="btn btn-danger">Delete</button>
+</form>
+</td>
 <?php } ?>
 
 </tr>
+
+<?php } ?>
+
 </table>
 </div>
